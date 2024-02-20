599ad191d29e4874b569dde873782dda837eb86b
naveen965 <39030430+naveen965@users.noreply.github.com>
2024-02-20 16:15:57 +0530

    Added Home.js to the projetc

--- finance-app/src/App.js
+++ finance-app/src/App.js
@@ -2,16 +2,47 @@ import React from 'react';
 import './App.css';
 import Login from './authentication/Login';
 import Register from './authentication/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+// import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) => 
+//       isAuthenticated ? (
+//         <Component {...props} />
+//       ) : (
+//         <Navigate to="/auth" />
+//       )
+//     }
+//   />
+// );
+
+// const PublicRoute = ({ component: Component, isAuthenticated, restricted, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) => 
+//       isAuthenticated && restricted ? (
+//         <Navigate to="/dashboard" />
+//       ) : (
+//         <Component {...props} />
+//       )
+//     }
+//   />
+// );
 
 const App = () => {
+  //const isAuthenticated = false;
+
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
+        {/* <Route exact path='/login' element={<Login />}></Route>
+        <Route exact path='/register' element={<Register />}></Route> */}
+        <Route path='/' element={<Home/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
---
+++ finance-app/src/Home.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { PropTypes } from 'prop-types';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import AppAppBar from './components/AppAppBar';
+
+const defaultTheme = createTheme({});
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Platform"
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>
+          {/* <SvgMaterialDesign sx={{ fontSize: '20px', mr: 1 }} /> */}
+          Material Design
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
+}
+
+export default function Home() {
+  const [mode, setMode] = React.useState('dark');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  //const LPtheme = createTheme(getLPtheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? defaultTheme : "hello"}>
+      <CssBaseline/>
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Box sx={{ bgcolor: 'background.default' }}></Box>
+      <ToggleCustomTheme
+        showCustomTheme={showCustomTheme}
+        toggleCustomTheme={toggleCustomTheme}
+      />
+    </ThemeProvider>
+  );
+}
